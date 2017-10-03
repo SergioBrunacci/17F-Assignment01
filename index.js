cd4be927404efe088cc1da13d77c70283afb3a20
Sergio de Almeida Brunacci <sergiodealmeidabrunacci@Sergios-MacBook-Pro.local>
2017-10-03 19:54:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+var SERVER_NAME = 'patient-api'
+var PORT = 8000;
+var HOST = '127.0.0.1';
+
+
+var restify = require('restify')
+
+  // Get a persistence engine for the patients
+  , patientsSave = require('save')('patients')
+
+  // Create the restify server
+  , server = restify.createServer({ name: SERVER_NAME})
+
+  server.listen(PORT, HOST, function () {
+  console.log('Server %s listening at %s', server.name, server.url)
+  console.log('Resources:')
+  console.log(' /patients')
+  console.log(' /patients/:id')  
+})
+
+server
+  // Allow the use of POST
+  .use(restify.fullResponse())
+
+  // Maps req.body to req.params so there is no switching between them
+  .use(restify.bodyParser())
+
+// Get all patients in the system
+server.get('/patients', function (req, res, next) {
+
+  // Find every entity within the given collection
+  patientsSave.find({}, function (error, patients) {
+
+    // Return all of the patients in the system
+    res.send(patients)
+  })
+})
+
+// Get a single patient by their patient id
+server.get('/patients/:id', function (req, res, next) {
+
+  // Find a single patient by their id within save
+  patientsSave.findOne({ _id: req.params.id }, function (error, patient) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    if (patient) {
+      // Send the patient if no issues
+      res.send(patient)
+    } else {
+      // Send 404 header if the patient doesn't exist
+      res.send(404)
+    }
+  })
+})
+
+// Create a new patient
+server.post('/patients', function (req, res, next) {
+
+  // Make sure name is defined
+  if (req.params.name === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('name must be supplied'))
+  }
+  if (req.params.age === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('age must be supplied'))
+  }
+  var newpatient = {
+		name: req.params.name, 
+		age: req.params.age
+	}
+
+  // Create the patient using the persistence engine
+  patientsSave.create( newpatient, function (error, patient) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send the patient if no issues
+    res.send(201, patient)
+  })
+})
+
+// Update a patient by their id
+server.put('/patients/:id', function (req, res, next) {
+
+  // Make sure name is defined
+  if (req.params.name === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('name must be supplied'))
+  }
+  if (req.params.age === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('age must be supplied'))
+  }
+  
+  var newpatient = {
+		_id: req.params.id,
+		name: req.params.name, 
+		age: req.params.age
+	}
+  
+  // Update the patient with the persistence engine
+  patientsSave.update(newpatient, function (error, patient) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send a 200 OK response
+    res.send(200)
+  })
+})
+
+// Delete patient with the given id
+server.del('/patients/:id', function (req, res, next) {
+
+  // Delete the patient with the persistence engine
+  patientsSave.delete(req.params.id, function (error, patient) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send a 200 OK response
+    res.send()
+  })
+})
+
+
